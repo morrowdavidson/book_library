dde2b28897bdef5fdeec35b05d2bfe48f811bbba
Morrow Davidson <morrow.davidson@gmail.com>
2019-01-26 21:06:48 -0800

    Add 'Add Book' feature

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 let myLibrary =[];
+let addBookButton = document.querySelector('#addBookButton');
+let addBookForm = document.querySelector('#addBookForm');
+let addBookFormButton = document.querySelector('#addBookFormButton');
+let cancelFormButton = document.querySelector('#cancelFormButton');
+
+addBookButton.onclick = function() {addBookForm.classList.remove("hidden")};
+cancelFormButton.onclick = function() {addBookForm.classList.add("hidden")};
+addBookFormButton.onclick = function() {
+    let title = document.querySelector("input[name='book_title']").value;
+    let author = document.querySelector("input[name='book_author']").value;
+    let num_of_pages = document.querySelector("input[name='book_pages']").value;
+    let read = document.querySelector("input[name='book_read']").checked;
+
+    addBookToLibrary(title,author,num_of_pages,read);
+
+    addBookForm.classList.add("hidden")
+};
 
 
 function Book(title,author,num_of_pages,read){    
@@ -20,19 +37,22 @@ Book.prototype.info = function(){
     return this.title + " by " + this.author + ", " + this.num_of_pages + " pages, " + this.readTxt();  
 }
 
-Book.prototype.tdBookTemplate = function(){
-    let template = "<tr>" +
+Book.prototype.tdBookTemplate = function(bookIndex){
+    let template = "<tr data-attribute=" + bookIndex + ">" +
                     "<td>" + this.title + "</td>" +
                     "<td>" + this.author + "</td>" +
                     "<td>" + this.pages + "</td>" +
-                    "<td>" + this.readTxt() + "</td>";
+                    "<td>" + this.readTxt() + "</td>" +
+                    "</tr>";
     return template;
 }
 
 function addBookToLibrary(title,author,num_of_pages,read) {
     let newBook = new Book(title,author,num_of_pages,read);
     myLibrary.push(newBook);
-    renderBook(newBook.tdBookTemplate(), document.querySelector('#myLibraryTable'));
+
+    let bookIndex = myLibrary.length - 1;
+    renderBook(newBook.tdBookTemplate(bookIndex), document.querySelector('#myLibraryTable'));
 
     return myLibrary;
 }
@@ -44,6 +64,3 @@ function renderBook(template, node){
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
 addBookToLibrary("The Hobbit2", "J.R.R. Tolkien", 295, true);
-
-
-
